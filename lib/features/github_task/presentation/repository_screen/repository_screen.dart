44fb2a8c87af2db35b58e80a061d58f2321e27b9
amalnaroth <amalzenox129@gmail.com>
2024-01-11 06:30:50 +0530

    repository open broweser view completed

--- lib/features/github_task/presentation/repository_screen/repository_screen.dart
+++ lib/features/github_task/presentation/repository_screen/repository_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:github_task/core/utils/size_utils.dart';
 import 'package:github_task/features/github_task/infrastructure/entites/git_repository_entity.dart';
 import 'package:github_task/features/github_task/presentation/widgets/text_widget.dart';
+import 'package:url_launcher/link.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class RepositoryScreen extends StatelessWidget {
@@ -9,44 +10,66 @@ class RepositoryScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final args = ModalRoute.of(context)!.settings.arguments as List<RepositoryEntity>;
+    final args =
+        ModalRoute.of(context)!.settings.arguments as List<RepositoryEntity>;
     return Scaffold(
       appBar: AppBar(
         title: Row(
           children: [
-            TextWidget(textValue: "Repository",
-            fontSize: 18,
-            fontWeight: FontWeight.bold),
+            TextWidget(
+                textValue: "Repository",
+                fontSize: 18,
+                fontWeight: FontWeight.bold),
             fWidth10,
-            Icon(Icons.work_outline,
-            size: 25,)
+            Icon(
+              Icons.work_outline,
+              size: 25,
+            )
           ],
         ),
         centerTitle: true,
       ),
       body: ListView.separated(
-        padding: EdgeInsets.all(20),
-        itemBuilder: (context, index) {
-          final data = args[index];
-        return ListTile(
-          onTap: () async{
-            final Uri url = Uri.parse('https://flutter.dev');
-   if (!await launchUrl(url)) {
-        throw Exception('Could not launch $_url');
-    }
+          padding: EdgeInsets.all(20),
+          itemBuilder: (context, index) {
+            final data = args[index];
+            return args.isEmpty
+                ? Center(
+                    child: TextWidget(textValue: "The repository is empty"),
+                  )
+                : Link(
+                  uri: Uri.parse(args[index].htmlUrl ?? ""),
+                  target: LinkTarget.defaultTarget,
+                  builder: (context, followLink) {
+                    return ListTile(
+                      onTap: () => launchUrl(Uri.parse(args[index].htmlUrl ?? "",),
+                      mode: LaunchMode.inAppWebView
+                      ),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                      leading: CircleAvatar(
+                        child: Center(
+                          child: Text(data.language ?? "",
+                              textAlign: TextAlign.center),
+                        ),
+                        radius: mWidth! * .09,
+                      ),
+                      tileColor: Colors.grey.withOpacity(.4),
+                      title:
+                          TextWidget(textValue: data.name! ?? "", fontSize: 18),
+                      subtitle: TextWidget(textValue: data.visibility! ?? ""),
+                      trailing: Icon(
+                        Icons.arrow_forward_ios,
+                        color: Colors.white,
+                      ),
+                    );
+                  },
+                );
           },
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(10)
-          ),
-          leading: CircleAvatar(child: Center(child: TextWidget(textValue: data.language ?? ""))),
-          tileColor: Colors.grey.withOpacity(.4),
-          title: TextWidget(textValue: data.name! ?? ""),
-          subtitle: TextWidget(textValue: data.visibility! ?? ""),
-          trailing: TextWidget(textValue: data.pushedAt! ?? ""),
-        );
-      }, separatorBuilder: (context, index) {
-        return fHight20;
-      }, itemCount: args.length ?? 0),
+          separatorBuilder: (context, index) {
+            return fHight20;
+          },
+          itemCount: args.length ?? 0),
     );
   }
 }
